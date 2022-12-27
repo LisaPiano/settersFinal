0e399440ee9556af10069f0e80f241af3f53fb86
Lisa Campi Walters <campi_l@fortlewis.edu>
2022-12-26 20:03:38 -0700

    added a Student class and a getter

--- settersFinal.js
+++ settersFinal.js
@@ -1,9 +1,35 @@
-class Pie{
-    constructor(flavor, size, spiceLevel){
-    this.flavor = flavor;
-    this.size = size;
-    this.spiceLevel = spiceLevel;
+// class Pie{
+//     constructor(flavor, size, spiceLevel){
+//     this.flavor = flavor;
+//     this.size = size;
+//     this.spiceLevel = spiceLevel;
+// }
+// }
+// const apple = new Pie('apple', 'medium', 'hot');
+// console.log(apple);
+
+class Student {
+    constructor(gpa, credits){
+        this.gpa = gpa;
+        this.credits = credits;
+    }
+
+    stringGPA() {
+        return this.gpa.toString();
+    }
+  get level() {
+    if(this.credits > 90){
+      return "Senior";
+    } else if (this.credits >=61 && this.credits <= 90) {
+      return "Junior"; 
+    } else if (this.credits >30 && this.credits <=60) {
+      return "Sophomore";
+    } else {
+      return "Freshman";
+    }
+  }
 }
-}
-const apple = new Pie('apple', 'medium', 'hot');
-console.log(apple);
+
+const student = new Student(3.9, 40);
+
+console.log(student.level);
